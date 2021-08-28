2753c2b358e4cfa4a213f3f0176ae4958778b30e
quangdung93 <quangdung16393@gmail.com>
2021-08-28 11:42:43 +0700

    Improve

--- resources/views/themes/kangen/homepage/index.blade.php
+++ resources/views/themes/kangen/homepage/index.blade.php
@@ -517,8 +517,40 @@
         </div>
     </section>
 
-    <section>
-        
+    <section class="call-to-action">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                    <div class="left">
+                        <div class="icon-hand-oke"><img width="50" src="{{ asset('themes/kangen/images/hand-two-finger.svg') }}" alt="Icon oke"/></div>
+                        <div class="content">
+                            <h4 class="text-white">Hãy Bảo Vệ Sức Khỏe Cả Gia Đình Cùng Kangen</h4>
+                            <p class="text-white m-0">Để lại thông tin, chúng tôi sẽ liên hệ lại ngay khi có thể.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <a href="#" class="btn read-more mt-4"><i class="feather icon-chevrons-right"></i> Tìm hiểu thêm</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-products">
+        <div class="container">
+            <div class="section-title">
+                <div class="icon">
+                    <i class="feather icon-award"></i>
+                </div>
+                <h3><a href="#">LỢI ÍCH CỦA MÁY LỌC NƯỚC KANGEN</a></h3>
+            </div>
+            <div class="content">
+                <div class="row">
+                    
+                </div>
+                <a href="#" class="btn read-more mt-4">Xem thêm <i class="feather icon-chevrons-right"></i></a>
+            </div>
+        </div>
     </section>
 
 @endsection
