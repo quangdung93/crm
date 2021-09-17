42a9073a1c28807e0cbc8528b23cd02e953a1b86
quangdung93 <quangdung16393@gmail.com>
2021-09-17 16:19:02 +0700

    Fix

--- resources/views/themes/kangen/master.blade.php
+++ resources/views/themes/kangen/master.blade.php
@@ -4,9 +4,8 @@
     <title>{{ $metaData['title'] ?? setting('site_title') }} | Kangen Việt Nam</title>  
     <!-- Meta -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, maximum-scale=1.0, user-scalable=0, initial-scale=1.0" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, maximum-scale=1.0, user-scalable=0, initial-scale=1.0" />
     <meta name="robots" content="index, follow" />
     <meta name="description" content="{{ $metaData['description'] ?? setting('site_description') }}" />
     <meta name="keywords" content="{{ $metaData['keyword'] ?? setting('site_keyword') }}" />
