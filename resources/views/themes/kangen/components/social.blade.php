c143168092eb818e4327c067718941496ff56ad7
quangdung93 <quangdung16393@gmail.com>
2021-09-27 17:07:26 +0700

    Fix zalo url

--- resources/views/themes/kangen/components/social.blade.php
+++ resources/views/themes/kangen/components/social.blade.php
@@ -1,18 +1,15 @@
 @php
     $facebook = setting('site_facebook_link');
-    $google = setting('site_google_plus');
-    $twitter = setting('site_twitter');
-    $instagram = setting('site_instagram');
-    $youtube = setting('site_youtube');
+    $zalo = setting('site_zalo');
 @endphp
 <div class="social">
     @if($facebook)
         <a href="{{ $facebook }}" class="item-social btn-facebook"><i class="feather icon-facebook"></i></a>
     @endif
 
-    @if($google)
-    <a href="{{ $google }}" class="item-social btn-zalo">
-        <img width="40" height="40" src="{{ asset('themes/kangen/images/zalo.png') }}" alt="Zalo" />
-    </a>
+    @if($zalo)
+        <a href="{{ $zalo }}" class="item-social btn-zalo">
+            <img width="40" height="40" src="{{ asset('themes/kangen/images/zalo.png') }}" alt="Zalo" />
+        </a>
     @endif
 </div>
