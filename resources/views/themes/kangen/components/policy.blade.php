a52738f8b911e8960c29da6e61eceac063fef4af
DũngVQ <quangdung16393@gmail.com>
2021-11-29 14:33:57 +0700

    Edit url

--- resources/views/themes/kangen/components/policy.blade.php
+++ resources/views/themes/kangen/components/policy.blade.php
@@ -7,19 +7,19 @@
                     <img class="image lazy" data-src="{{ asset('assets/images/policy-icon-1.png') }}" alt="">
                     <div class="policy-item-title">Sản phẩm chất lượng</div>
                     <div class="policy-item-des text-center">Công ty trực tiếp phân phối dòng máy điện giải kangen từ tập đoàn Enagic Nhật Bản đứng top đầu thế giới.</div>
-                    <a href="{{ url('/san-pham') }}" class="btn btn-kangen">Xem</a>
+                    <a href="{{ url('/may-loc-nuoc-kangen') }}" class="btn btn-kangen">Xem</a>
                 </div>
                 <div class="policy-item">
                     <img class="image lazy" data-src="{{ asset('assets/images/policy-icon-2.png') }}" alt="">
                     <div class="policy-item-title">Giá cả cạnh tranh</div>
                     <div class="policy-item-des text-center">Đưa ra nhiều sự lựa chọn cho khách hàng với giá thành ưu đãi nhất, chất lượng nhất.</div>
-                    <a href="{{ url('/tin-tuc/bang-gia-top-6-may-loc-nuoc-kangen-nhat-ban') }}" class="btn btn-kangen">Xem</a>
+                    <a href="{{ url('/kinh-nghiem-hay/gia-may-loc-nuoc-kangen') }}" class="btn btn-kangen">Xem</a>
                 </div>
                 <div class="policy-item">
                     <img class="image lazy" data-src="{{ asset('assets/images/policy-icon-3.png') }}" alt="">
                     <div class="policy-item-title">Vận chuyển an toàn</div>
                     <div class="policy-item-des text-center">Vận chuyển miễn phí ở khu vực thành phố HCM, Hà Nội và các tỉnh lân cận trên toàn quốc.</div>
-                    <a href="{{ url('/dich-vu-kangen-dai-lam-thinh') }}" class="btn btn-kangen">Xem</a>
+                    <a href="{{ url('/dich-vu-kangen') }}" class="btn btn-kangen">Xem</a>
                 </div>
             </div>
         </div>
